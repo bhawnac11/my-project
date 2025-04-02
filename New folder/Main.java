9f6206d5c73b4c1acfdb0bc7c35eeeb3b6ecb4c9
Bhawna <bhawnac11@gmail.com>
2025-04-02 14:02:18 +0530

    this is my first attempt

---
+++ New folder/Main.java
@@ -0,0 +1,18 @@
+class Main{
+
+public static void main(String[]args){
+Main ob1=new Main();
+int Output=ob1.sum(12,12);
+System.out.println("Output:" + Output);
+
+}
+
+public int sum(int x,int y){
+int z=x+y;
+System.out.println(z);
+return z;
+
+}
+
+
+}
